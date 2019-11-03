1647de7f6d99e814619dc3ca991d47f2681e146f
harukazu <harumasa456@gmail.com>
2019-11-03 09:25:02 +0900

    課題2の再提出です
    要素が4つしかないの意味がわからなかったので、とりあえず順番がバラバラになってしまう問題を修正しました

---
+++ Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+
+
+
+    private void Start()
+    {
+        int array;
+        {
+            int[] points = new int[6];
+            points[0] = 47;
+            points[1] = 86;
+            points[2] = 43;
+            points[3] = 19;
+            points[4] = 67;
+            points[5] = 57;
+            for (int num = 0; num <= 5; num++) { Debug.Log(points[num]); };
+            for (int i = 5; i >= 0; i--) { Debug.Log(points[i]); }
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
+
+
+
